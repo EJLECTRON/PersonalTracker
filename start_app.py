d238dea38404ea7db1bd7388058d823959569404
sashamikushova <sasha.mikushova@gmail.com>
2023-07-31 13:05:00 +0300

    Update start_app.py

--- start_app.py
+++ start_app.py
@@ -1,24 +1,27 @@
+#TO DO: Add - _alert_message: MessageAlert
 from PyQt5.QtWidgets import QWidget
-
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import QPoint
 from PyQt5 import QtWidgets
 from ui_startInterface import Ui_Form
-from start_model import StartModel
+from start_controller import StartController
+
 
 class StartWindow(QtWidgets.QWidget):
     """ Custom class for start window"""
 
     def __init__(self):
         QWidget.__init__(self)
+        QtWidgets.QWidget.__init__(self)
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.old_pos = self.pos()
+        self.controller = StartController(self)
 
         try:
             self.__button_actions()
-            self.__hide_tool_tip()
-            self.__show_quote()
+            self.controller.hide_tool_tip()
+            self.controller.show_quote()
         except Exception as error:
             print(error)
 
@@ -34,67 +37,10 @@ class StartWindow(QtWidgets.QWidget):
 
     def __button_actions(self):
         """ setting up all actions for buttons"""
-        self.ui.newUserBtn.clicked.connect(self.__switch_to_new_user)
+        self.ui.newUserBtn.clicked.connect(self.controller.switch_to_new_user)
+        self.ui.closeWindowBtn.clicked.connect(self.controller.close_window)
+        self.ui.collapseWindowBtn.clicked.connect(self.controller.hide_window)
+        self.ui.signInBtn.clicked.connect(self.controller.log_in)
+        self.ui.createAccountBtn.clicked.connect(self.controller.sign_up)
+        self.ui.returnToMenuBtn.clicked.connect(self.controller.switch_to_log_in)
 
-        self.ui.closeWindowBtn.clicked.connect(self.__close_window)
-        self.ui.collapseWindowBtn.clicked.connect(self.__hide_window)
-        self.ui.signInBtn.clicked.connect(self.__log_in)
-        self.ui.createAccountBtn.clicked.connect(self.__sign_up)
-        self.ui.returnToMenuBtn.clicked.connect(self.__switch_to_log_in)
-
-    def __show_quote(self):
-        """shows random quote from quotes.start_quotes to a top right corner"""
-        quote = StartModel().get_quote()
-
-        self.ui.quoteLabel.setText(quote)
-
-# -----------buttons functions------------------------------------
-    def __switch_to_new_user(self):
-        """ switch to new user window"""
-        self.ui.mainBody.setCurrentIndex(1)
-
-    def __switch_to_log_in(self):
-        """ switch to log in window"""
-        self.ui.mainBody.setCurrentIndex(0)
-
-    def __hide_tool_tip(self):
-        """ hide tool tip"""
-        self.setWindowFlag(Qt.FramelessWindowHint)
-
-    def __hide_window(self):
-        """ hide window"""
-        self.showMinimized()
-
-    def __close_window(self):
-        """ close window"""
-        self.close()
-
-    def __close_window_open_main_window(self):
-        self.close()
-
-    def __log_in(self):
-        user_name = self.ui.logInLineEdit.text()
-        password = self.ui.passwordLineEdit.text()
-
-        start = StartModel(user_name, password)
-        result = None
-
-        if start.is_correct_data_for_log_in():
-            result = start.try_to_log_in()
-
-        if result is not None:
-            self.__close_window_open_main_window()
-        else:
-            QtWidgets.QMessageBox.about(self, "Error", "User not found, try again or create new user")
-            print(f"User {user_name} not found")
-
-
-    def __sign_up(self):
-        user_name = self.ui.usernameLineEdit.text()
-        password = self.ui.newPasswordLineEdit.text()
-        repeated_password = self.ui.repeatPasswordLineEdit.text()
-
-        start = StartModel(user_name, password, repeated_password)
-
-        if start.is_correct_data_for_sign_up():
-            start.try_to_sign_up()
